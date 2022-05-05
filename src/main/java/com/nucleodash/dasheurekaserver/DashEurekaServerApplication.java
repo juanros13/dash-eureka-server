35b1f9e9d898f5ae328624c590c9c6df111861d2
juanros13 <juanros13@gmail.com>
2022-05-04 23:59:10 -0500

    first commit

---
+++ src/main/java/com/nucleodash/dasheurekaserver/DashEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.nucleodash.dasheurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nucleodash/dasheurekaserver/DashEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nucleodash.dasheurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
