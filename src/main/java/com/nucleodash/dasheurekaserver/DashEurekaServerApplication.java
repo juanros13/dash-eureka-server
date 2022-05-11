a07613685291053e70db3f1baca2427473c670dd
juan roberto <juanros13@gmail.com>
2022-05-11 10:09:25 -0500

    Eureka config

--- src/main/java/com/nucleodash/dasheurekaserver/DashEurekaServerApplication.java
+++ src/main/java/com/nucleodash/dasheurekaserver/DashEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.nucleodash.dasheurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DashEurekaServerApplication {
 
 	public static void main(String[] args) {
